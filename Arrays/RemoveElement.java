ed5c95b62f1e0dbef54a8fb95a20f02e64433866
MallemAli <42662365+MallemAli@users.noreply.github.com>
2021-11-27 16:55:28 -0500

    Create RemoveElement.java

---
+++ Arrays/RemoveElement.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+    /*  Approach 1
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+    */    
+    
+        
+    //Approach 2;  when the element to remove is rare, this is more efficient   
+    int i = 0;
+    int n = nums.length;
+    while (i < n) {
+        if (nums[i] == val) {
+            nums[i] = nums[n - 1];
+            // reduce array size by one
+            n--;
+        } else {
+            i++;
+        }
+    }
+    return n;    
+    }
+}
