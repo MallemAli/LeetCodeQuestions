6aa7d6fc37841ba0b8c703f6abd5452b8d4596c6
MallemAli <42662365+MallemAli@users.noreply.github.com>
2021-11-27 19:27:17 -0500

    Update RemoveDuplicateFromSortedArray.java

--- Arrays/RemoveDuplicateFromSortedArray.java
+++ Arrays/RemoveDuplicateFromSortedArray.java
@@ -1,27 +1,17 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i = 0, k = 0;
-        
-        if(nums.length>=1){
-            k=1;
-        }
+        if(nums.length==0) return 0;
+        int i = 0;
         
         for(int j=1; j<nums.length; j++){
-            if(nums.length==1){
-                break;
-            } else if(nums.length==0){
-                break;
-            }
             
             if(nums[i]==nums[j]){
                 //increment j
             } else {
                 nums[++i]=nums[j];
-                k++;
             }
         }
         
-        System.out.println("k = "+ k);
-        return k;
+        return i+1;
     }
 }
